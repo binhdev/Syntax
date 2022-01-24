91b6baaae7206a6544d8d369c7929e04015f69e3
Thanh Binh <ttbinh@hueic.edu.vn>
2022-01-24 18:56:25 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,17 @@
+package basic.dev;
+/**
+ * 
+ * @author tranthanhbinh
+ *
+ */
+public class MainApp {
+	//Comment tren 1 dong
+	public static void main(String[] args) {
+		int number;
+		String nameSchool;
+	}
+	
+	public void show() {
+		
+	}
+}
